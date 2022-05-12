37101c37dda3ae36eaf80b3748d7503ac723452f
DenSmith1 <ntreid80@gmail.com>
2022-05-12 23:11:24 +0400

    Домашнее задание 2. Квадраты чисел

---
+++ Homework2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Домашнее задание 2. Квадрат чисел
+
+Console.Write("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+if (number2*number2 == number1)
+{
+    Console.Write(number1+" Является квадратом числа "+number2);
+}
+else if (number1*number1 == number2)
+{
+    Console.Write(number2+" Является квадратом числа "+number1);
+}
+else
+    Console.Write("Данные числа не являются квадратами друг друга");
