783a61a91e2754f53f0f968c3569309e9ec2afd6
DenSmith1 <ntreid80@gmail.com>
2022-05-13 00:11:52 +0400

    Домашнее задание 4. Расстояние между точек

---
+++ Homework4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Домашнее задание 4. Расстояние между точек в пространстве 2D/3D
+
+Console.WriteLine("Введите координаты точки А");                    // Вводими координаты точки А
+Console.Write("X: ");
+int xA = int.Parse(Console.ReadLine());
+Console.Write("Y: ");
+int yA = int.Parse(Console.ReadLine());
+Console.Write("Z (если пространство двухмерное, то введите 0): ");  // Если плоскость двухмерная, то координаты Z = 0 для А и B
+int zA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты точки B");                    // Вводими координаты точки B
+Console.Write("X: ");
+int xB = int.Parse(Console.ReadLine());
+Console.Write("Y: ");
+int yB = int.Parse(Console.ReadLine());
+Console.Write("Z (если пространство двухмерное, то введите 0): ");  // Если плоскость двухмерная, то координаты Z = 0 для А и B
+int zB = int.Parse(Console.ReadLine());
+double distance = Math.Sqrt(Math.Pow((xB-xA),2)+Math.Pow((yB-yA),2)+Math.Pow((zB-zA),2));
+Console.WriteLine("Расстояние между точками = "+distance); 
