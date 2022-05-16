87035d5aa1786bb21359063e1ed4583852e6b0d0
DenSmith1 <ntreid80@gmail.com>
2022-05-16 12:15:38 +0400

    Семинар 2. Произведение чисел

---
+++ Seminar2_4/Program.cs
@@ -0,0 +1,11 @@
+﻿// Произведение чисел
+Console.WriteLine("Введите число: ");
+int Number = int.Parse(Console.ReadLine() ?? "0");
+int i = 1;
+int Result = 1;
+while (i <= Number)
+{
+    Result = Result*i;
+    i++;
+}
+Console.WriteLine($"Произведение чисел от 1 до {Number} = {Result}");
