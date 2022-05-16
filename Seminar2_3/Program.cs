cad5a6bae5f8238ecc5076401f372d8be32f2bae
DenSmith1 <ntreid80@gmail.com>
2022-05-16 11:58:48 +0400

    Семинар 2. Количество цифр

---
+++ Seminar2_3/Program.cs
@@ -0,0 +1,10 @@
+﻿// Найти количество цифр
+Console.WriteLine("Введите число: ");
+int Number = int.Parse(Console.ReadLine() ?? "0");
+int digits = 1;
+while (Number > 9)
+{
+    Number = Number/10;
+    digits ++;
+}
+Console.WriteLine("Количество цифр: "+digits);
