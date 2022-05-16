211d16968122c86a791d07d18d12c5b38bd6fc2f
DenSmith1 <ntreid80@gmail.com>
2022-05-16 12:52:52 +0400

    Семинар 2. Создание массива

---
+++ Seminar2_5/Program.cs
@@ -0,0 +1,17 @@
+﻿// Создать массив из 8 элементов
+Console.WriteLine("Задать массив из 8 элементов и вывести их на экран");
+Console.Write("Введите размер массива: ");
+
+int Number = int.Parse(Console.ReadLine() ?? "0");
+int[] array = new int[Number];
+
+for (int i = 0; i < Number; i++)
+    array[i] = new Random().Next(0, 100);
+
+int count = 1;
+foreach (int k in array)
+{
+    Console.WriteLine($"{count} элемент массива - {k}");
+    count++;
+}
+
