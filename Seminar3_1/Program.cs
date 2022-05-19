925822d2219df4d612886f7c8c3dc2c436e631ee
DenSmith1 <ntreid80@gmail.com>
2022-05-19 13:35:56 +0400

    Семинар 3. Массивы

---
+++ Seminar3_1/Program.cs
@@ -0,0 +1,27 @@
+﻿//  Программа замену элементов массива на противоположные
+
+void PrintArray (int[]array)
+{
+    int count = array.Length;
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+Console.Write("Введите размер массива: ");
+
+int Number = int.Parse(Console.ReadLine() ?? "0");
+int[] arr = new int[Number];
+
+for (int i = 0; i < Number; i++)
+    arr[i] = new Random().Next(-100, 100);
+
+
+PrintArray (arr);
+Console.WriteLine ();
+
+for (int i = 0; i < arr.Length; i++)
+    arr [i] = -arr[i];
+
+PrintArray (arr);
+
---
+++ Seminar3_4/Program.cs
@@ -0,0 +1,25 @@
+﻿// Найдите разницу между максимальным и минимальным элементом
+
+Console.Write("Введите размер массива: ");
+
+int Number = int.Parse(Console.ReadLine() ?? "0");
+double[] arr = new double[Number];
+
+for (int i = 0; i < Number; i++)
+{
+    arr[i] = new Random().NextDouble() * 10;
+    arr[i] = Math.Round(arr[i],2);
+    Console.Write($"{arr[i]} ");
+}
+Console.WriteLine("");
+
+int min = 0;
+int max = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] < arr[min]) min = i;
+    else if (arr[i] > arr[max]) max = i;
+
+}
+
+Console.WriteLine($"Разница между {arr[max]} и {arr[min]} = {arr[max] - arr[min]}");
